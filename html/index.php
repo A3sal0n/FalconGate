24515b0ffd17017b30fb927265c62507e31897be
easy4MEr <easy4MEr@gmail.com>
2017-02-12 12:59:17 -0500

    Edited login form. JavaScript code added. When click into input field, default text will disappear.

--- html/index.php
+++ html/index.php
@@ -164,6 +164,17 @@ if (login_check() == true) {
     $logged = 'out';
 }
 ?>
+<script type="text/javascript">
+function fillField(input,val) {
+      if(input.value == "")
+         input.value=val;
+};
+
+function clearField(input,val) {
+      if(input.value == val)
+         input.value="";
+};
+</script>
 <table height="100%" width="100%">
 	<tr>
 		<td align="center" valign="center">
@@ -174,8 +185,8 @@ if (login_check() == true) {
             <h3>Login</h3>
             <fieldset>
                 <form action="includes/process_login.php" method="post" name="login_form">
-                    <input type="uname" name="uname" value="Username">
-                    <input type="password" name="passwd" value="Password">
+                    <input type="uname" name="uname" value="Username" onblur="fillField(this,'Username');" onfocus="clearField(this,'Username');"/>
+                    <input type="password" name="passwd" value="Password" onblur="fillField(this,'Password');" onfocus="clearField(this,'Password');"/>
                     <input type="submit" value="Login">
                     <footer class="error_message">
                            <?php
