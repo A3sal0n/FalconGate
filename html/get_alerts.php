6587bd504ba46ddd808129059809f451f20041ff
easy4MEr <easy4MEr@gmail.com>
2017-02-19 09:21:11 -0500

    Little GUI modifications, .css file edited

--- html/get_alerts.php
+++ html/get_alerts.php
@@ -21,6 +21,8 @@ $result = CallAPI('POST', 'http://127.0.0.1:5000/api/v1.0/falcongate/status', js
 if (!$result){
     echo ("<h3><span class=error_message>FalconGate API process seems to be down!<span></h3>");
     echo ("<h3><span class=error_message>Check your device's configuration and reboot if necessary.</span></h3>");
+    echo ("<h3>FalconGate API process seems to be down!</h3>");
+    echo ("<h3>Check your device's configuration and reboot if necessary.</h3>");
 }else{
     $obj = json_decode($result, true);
     echo ("<h3>Alerts detected in the previous week</h3>");
