6653a805c397afe06dc7946247a2185913fe40c4
nexus <Leonardo Mokarzel Falcon>
2019-08-16 17:32:20 +0200

    Merging install scripts

--- install/Ubuntu/easy_install.sh
+++ install/Ubuntu/easy_install.sh
@@ -2,6 +2,26 @@
 
 set -e
 
+# Check if script has root privs
+if [ "$(whoami)" != "root" ]; then
+	echo "Sorry, you are not root."
+	exit 1
+fi
+
+echo "Updating system software..."
+sleep 3
+
+add-apt-repository ppa:shevchuk/dnscrypt-proxy
+
+apt-get update && apt-get upgrade -y
+
+echo "Installing software dependencies..."
+sleep 3
+
+apt-get install cmake make gcc g++ flex bison libpcap-dev libssl-dev libffi-dev dialog python-dev swig zlib1g-dev libgeoip-dev build-essential libelf-dev dnsmasq nginx php-fpm php-curl mailutils ipset git python3-pip python3-venv dnscrypt-proxy nmap hydra -y
+
+# Allow user to choose deployment mode
+MODE=""
 HEIGHT=15
 WIDTH=40
 CHOICE_HEIGHT=4
@@ -23,11 +43,13 @@ CHOICE=$(dialog --clear \
 clear
 case $CHOICE in
         1)
-            echo "You chose Option 1"
+            MODE="attached"
             ;;
         2)
-            echo "You chose Option 2"
+            MODE="router"
             ;;
 esac
+
+echo $MODE
 
 exit
